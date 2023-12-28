3fffda2205f96ab20b4a606583d5a574b3917de6
shane2528 <ussr2528@gmail.com>
2023-12-29 04:23:15 +0530

    first commit

---
+++ collectdata.py
@@ -0,0 +1,71 @@
+import cv2
+import os
+
+
+directory= 'SignImage48x48/'
+print(os.getcwd())
+
+if not os.path.exists(directory):
+    os.mkdir(directory)
+if not os.path.exists(f'{directory}/blank'):
+    os.mkdir(f'{directory}/blank')
+    
+
+
+
+
+
+
+import os
+import cv2
+cap=cv2.VideoCapture(0)
+while True:
+    _,frame=cap.read()
+    count = {
+             'hi': len(os.listdir(directory+"/HI")),
+             'i love u': len(os.listdir(directory+"/I LOVE U")),
+             'call me': len(os.listdir(directory+"/CALL ME")),
+             'peace': len(os.listdir(directory+"/PEACE")),
+             'good luck': len(os.listdir(directory+"/GOOD LUCK")),
+             'a': len(os.listdir(directory+"/A")),
+             'b': len(os.listdir(directory+"/B")),
+             'c': len(os.listdir(directory+"/C")),
+             'd': len(os.listdir(directory+"/D")),
+             'e': len(os.listdir(directory+"/E")),
+             'blank': len(os.listdir(directory+"/blank"))
+             }
+
+    row = frame.shape[1]
+    col = frame.shape[0]
+    cv2.rectangle(frame,(0,40),(300,300),(255,255,255),2)
+    cv2.imshow("data",frame)
+    frame=frame[40:300,0:300]
+    cv2.imshow("ROI",frame)
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame = cv2.resize(frame,(48,48))
+    interrupt = cv2.waitKey(10)
+    if interrupt & 0xFF == ord('h'):
+        cv2.imwrite(os.path.join(directory+'HI/'+str(count['hi']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('i'):
+        cv2.imwrite(os.path.join(directory+'I LOVE U/'+str(count['i love u']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('l'):
+        cv2.imwrite(os.path.join(directory+'CALL ME/'+str(count['call me']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('p'):
+        cv2.imwrite(os.path.join(directory+'PEACE/'+str(count['peace']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('g'):
+        cv2.imwrite(os.path.join(directory+'GOOD LUCK/'+str(count['good luck']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('a'):
+        cv2.imwrite(os.path.join(directory+'A/'+str(count['a']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('b'):
+        cv2.imwrite(os.path.join(directory+'B/'+str(count['b']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('c'):
+        cv2.imwrite(os.path.join(directory+'C/'+str(count['c']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('d'):
+        cv2.imwrite(os.path.join(directory+'D/'+str(count['d']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('e'):
+        cv2.imwrite(os.path.join(directory+'E/'+str(count['e']))+'.jpg',frame)
+    if interrupt & 0xFF == ord('.'):
+        cv2.imwrite(os.path.join(directory+'blank/' + str(count['blank']))+ '.jpg',frame)
+
+
+    
